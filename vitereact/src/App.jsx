d8f333bd610f8c7997c57e57409b037de1bd566c
Aarya3010 <123272783+Hustler-Aarya@users.noreply.github.com>
2025-03-22 19:45:51 +0530

    pushing file

---
+++ vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+// App.jsx
+import Aarya from "./element";  // Correctly import Aarya from element.jsx
+
+function App() {
+  return (
+    <h1>Tailwind Test</h1>
+    // <Aarya />  // Render the imported Aarya component
+  );
+}
+
+export default App;
---
+++ vitereact/src/element.jsx
@@ -0,0 +1,10 @@
+
+
+
+function Aarya(){
+    return(
+        <h1>hello how you doing</h1>
+    )
+};
+export default Aarya;
+
